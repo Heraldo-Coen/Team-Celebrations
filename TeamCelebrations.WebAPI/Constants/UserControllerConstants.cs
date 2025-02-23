ad899bea2d56cd728816a6de4509f0465cc1c496
Heraldo <79059454+Heraldo-Coen@users.noreply.github.com>
2025-02-22 19:23:21 -0500

    WebAPI: Update IUserCOntroller and UserCOntroller. Create PhoneCode Controller, Friendship Controller and Unit Controller. Create Constants

---
+++ TeamCelebrations.WebAPI/Constants/UserControllerConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace TeamCelebrations.WebAPI.Constants
+{
+    public static class UserControllerConstants
+    {
+        // Constants for locking user account
+        public const int LOG_IN_UNLOCK_MINUTES = 10;
+        public const int MAX_LOGIN_ATTEMPTS = 3;
+
+        // Constants for verification code
+        public const int VERIFICATION_CODE_EXPIRED_MINUTES = 5;
+        public const int VERIFICATION_MIN_RANGE_VALUE = 50000000;
+        public const int VERIFICATION_MAX_RANGE_VALUE = 99999999;
+        public const int VERIFICATION_CODE_NULL_VALUE = 10000000;
+        public const int RESET_PASSWORD_UNLOCK_MINUTES = 60;
+        public const int MAX_RESET_PASSWORD_ATTEMPTS = 3;
+
+        public const string EMAIL_ALREADY_USED_EXCEPTION_MESSAGE_1 = "23505: duplicate key value violates unique constraint";
+        public const string EMAIL_ALREADY_USED_EXCEPTION_MESSAGE_2 = "IX_Employees_Email";
+    }
+}
---
+++ TeamCelebrations.WebAPI/Controllers/UnitController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Security.Cryptography;
+using TeamCelebrations.Data.DataAccess;
+using TeamCelebrations.Data.Entities;
+using TeamCelebrations.Data.Requests;
+using TeamCelebrations.Data.Responses;
+
+namespace TeamCelebrations.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnitController(DataContext dataContext) : CustomControllerBase(dataContext)
+    {
+        [HttpPost]
+        [Route("Create")]
+        public async Task<ActionResult> Create(UnitRequest unitRequest)
+        {
+            try
+            {
+                if (unitRequest.HigherUnitId != Guid.Empty)
+                {
+                    // Verificar si el HigherUnitId existe en la tabla Units
+                    var higherUnitExists = await _dataContext.Units!.AnyAsync(u => u.Id == unitRequest.HigherUnitId);
+                    if (!higherUnitExists)
+                    {
+                        return BadRequest(new { message = "Invalid HigherUnitId." });
+                    }
+                }
+
+                await _dataContext.Units!.AddAsync(new Unit()
+                {
+                    Name = unitRequest.Name,
+                    //HigherUnitId = unitRequest.HigherUnitId
+                });
+
+                await _dataContext.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch (DbUpdateException ex) when (ex.InnerException != null)
+            {
+                return Conflict(new { message = "Foreign key constraint violated." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+    }
+}
